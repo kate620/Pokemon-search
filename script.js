456b924e8e2846e1cc2b56b7c2da2d8ad722134d
Donghao Wu <donghao.wu.us@gmail.com>
2022-05-10 23:58:50 -0400

    danny first commit.

--- script.js
+++ script.js
@@ -1,35 +1,40 @@
 // button
-document.querySelector("#search").addEventListener("click", getPokeMon);
+document.querySelector('#search').addEventListener('click', getAlbums);
+document.querySelector('#artistName').addEventListener('keypress', (e) => {
+  if (e.key === 'Enter') {
+    getAlbums();
+  }
+});
 
 //transform string in to all lowercase letter
 function lowerCaseName(string) {
   return string.toLowerCase();
 }
-// Captallize First letter
-function capitalizeFirstLetter(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
-}
 
 // Fetch data
-function getPokeMon(event) {
-  const name = document.querySelector("#pokemonName").value;
-  const pokemonName = lowerCaseName(name);
+function getAlbums(event) {
+  const name = document.querySelector('#artistName').value;
+  const artistName = lowerCaseName(name);
 
-  fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+  fetch(
+    `https://itunes.apple.com/search?term=${artistName}&media=music&entity=album&attribute=artistTerm&limit=200`
+  )
     .then((response) => response.json())
     .then((data) => {
-      document.querySelector(".pokemonBox").innerHTML = `<div>
-      <img
-        src="${data.sprites.other["official-artwork"].front_default}"
-        alt=${capitalizeFirstLetter(data.name)}
-      />
-    </div>
-    <div class="pokemonInfo">
-      <h1>${capitalizeFirstLetter(data.name)}</h1>
-      <p>Wight: ${data.weight}</p>
-    </div>`;
+      let albumContent = document.querySelector('.albumContent');
+      data.results.forEach((album) => {
+        let albumChildDiv = document.createElement('div');
+        albumChildDiv.innerHTML = `<div class="single-album">
+          <img
+          src=${album.artworkUrl100}
+          alt=${album.collectionName}
+        />
+        <p>${album.collectionName}<p>
+        </div>`;
+        albumContent.appendChild(albumChildDiv);
+      });
     })
     .catch((err) => {
-      console.log("Pokemon not found", err);
+      console.log('Pokemon not found', err);
     });
 }
